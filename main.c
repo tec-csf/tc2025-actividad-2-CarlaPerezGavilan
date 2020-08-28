07844789fbfbe1bd7ef2971540703240c5a05f78
Carla PGavilan <carlapgc@CarlaPGavilans-iMac.local>
2020-08-28 14:33:03 -0500

    new version

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+//
+//  main.c
+//  ejercicio actividad 2 - iterador 
+//
+//  Created by Carla Pérez Gavilán on 27/08/20.
+//  Copyright (c) 2020 Carla Pérez Gavilán. All rights reserved.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,59 +18,88 @@ typedef struct{
     void * current;
 } vector;
 
-typedef void * (*begin_t)(vector array);
+typedef void * (*iterator_t)(vector * array);
 
-typedef void * (*end_t)(vector array);
 
-typedef void * (*next_t)(vector array); 
+typedef struct{
+    iterator_t begin;
+    iterator_t end;
+    iterator_t next;
+    iterator_t prev;
+}iterator;
 
-typedef void * (*prev_t)(vector array); 
+typedef iterator * (*begin_t)(vector * array);
 
-void * begin(vector array){
-    return array.first;
+
+void * begin(vector * array){
+    return array->first;
 }
 
-void * end(vector array){
-    return array.first+(array.count*array.size);
+void * end(vector * array){
+    return array->first+(array->count*array->size)-array->size;
 }
 
-void * next(vector array){
-    if(array.current == end(array)){
+void * next(vector * array){
+    if(array->current == end(array)){
         return NULL;
     }else{
-        array.current= array.current+array.size;
-        return array.current;
+        return array->current + array->size;
     }
 }
 
-void * prev(vector array){
-    if(array.current == begin(array)){
+void * prev(vector * array){
+    if(array->current == begin(array)){
         return NULL;
     }else{
-        array.current= array.current-array.size;
-        return array.current;
+        return array->current - array->size;
     }
     
 }
 
-typedef struct{
-    begin_t begin;
-    end_t end;
-    next_t next;
-}iterator;
 
-void recorre(iterator it, vector array){
+
+iterator * forward(vector * array){
+    iterator it;
     it.begin = begin;
     it.end = end;
     it.next = next;
-    
-    printf("this is first element %s", it.begin(array));
-    printf("this is last element %s", it.end(array));
+    array->current = it.begin(array);
+    return &it;
+}
 
-    for(void * aux = it.begin(array); aux<it.end(array); it.next(array)){
-        printf(" element %s", aux);
+iterator * reverse(vector * array){
+    iterator it; 
+    it.begin = end;
+    it.end = begin;
+    it.next = prev;
+    array->current = it.begin(array);
+    return &it;
+}
+
+iterator * bidirect(vector * array){
+    iterator it;
+    it.begin = begin;
+    it.end = end;
+    it.next = next;
+    it.prev = prev;
+    array->current = it.begin(array);
+    return &it;
+}
+
+iterator * init(begin_t initialize, vector * array){
+    return (*initialize)(array);
+}
+
+void recorre(iterator it, vector array){
+    int count = 0;
+   for(int * aux = it.begin(&array); it.next(&array) != NULL; aux = it.next(&array)){
+        array.current = aux;
+        printf(" %4d es el ìndice %4d \n", *aux, count);
+        count++;
     }
 }
+
+
 
 int main(int argc, const char * argv[])
 {
@@ -73,16 +110,17 @@ int main(int argc, const char * argv[])
     enteros.size = sizeof(int);
 
     int * aux = enteros.first;
-    int * last = enteros.first + N;
+    int * last = enteros.first + (N*sizeof(int));
 
     for (; aux < last; ++aux) {
         *aux = rand() % 100;
+        printf(" assigned %4d \n", *aux);
     }
 
-    iterator forward;
-    forward.begin = begin;
-    forward.end = end;
-    forward.next = next;
-    recorre(forward, enteros);
+    iterator it = *(init(&forward, &enteros));
+
+
+    recorre(it, enteros);
     
 }
+
